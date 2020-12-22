2af3b6aa2bf77aeb231723462e424523abf820e7
Slade <wiede_dani@bs-an.de>
2020-12-22 19:27:59 +0100

    hello memory my old friend

--- sladebot/view/pmd_personality_quiz.php
+++ sladebot/view/pmd_personality_quiz.php
@@ -8,8 +8,8 @@ $viewQuestions = new pmdFront(10)?>
 <body>
     <form id="questions">
         <h1>is this thing on?</h1>
-        <h1><?php var_dump($viewQuestions)?></h1>
-        <?php foreach ($viewQuestions as $viewQuestion) {?>
+        <h1><?php var_dump($viewQuestions);?></h1>
+        <?php foreach ($viewQuestions->getQuestionArray() as $viewQuestion) {?>
         <h2><?= $viewQuestion["question"]?></h2><br>
         <?php foreach ($viewQuestion["answers"] as $answer) {?>
         <input type ="radio" id="<?=$answer?>"><label for="<?=$answer?>"><?=$answer?></label><br>
