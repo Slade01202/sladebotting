08366f9b5fbe6bd1346f27eabe9932b28354b9e3
Slade <wiede_dani@bs-an.de>
2020-12-22 19:30:12 +0100

    BAB

--- sladebot/view/pmd_personality_quiz.php
+++ sladebot/view/pmd_personality_quiz.php
@@ -1,21 +1,21 @@
 <?php require_once(__DIR__ . "/../frontend/pmd_personality_quiz/pmd_personality_front.php");
-$viewQuestions = new pmdFront(10)?>
+$viewQuestions = new pmdFront(1)?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>PMD Personality Quiz</title>
-</head>
-<body>
-    <form id="questions">
-        <h1>is this thing on?</h1>
-        <h1><?php var_dump($viewQuestions);?></h1>
-        <?php foreach ($viewQuestions->getQuestionArray() as $viewQuestion) {?>
-        <h2><?= $viewQuestion["question"]?></h2><br>
-        <?php foreach ($viewQuestion["answers"] as $answer) {?>
-        <input type ="radio" id="<?=$answer?>"><label for="<?=$answer?>"><?=$answer?></label><br>
-        <?php }
-        }
-        ?>
-    </form>
-</body>
+    <head>
+        <title>PMD Personality Quiz</title>
+    </head>
+    <body>
+        <form id="questions">
+            <h1>is this thing on?</h1>
+            <h1><?php var_dump($viewQuestions);?></h1>
+            <?php foreach ($viewQuestions->getQuestionArray() as $viewQuestion) {?>
+            <h2><?= $viewQuestion["question"]?></h2><br>
+            <?php foreach ($viewQuestion["answers"] as $answer) {?>
+            <input type ="radio" id="<?=$answer?>"><label for="<?=$answer?>"><?=$answer?></label><br>
+            <?php }
+            }
+            ?>
+        </form>
+    </body>
 </html>
