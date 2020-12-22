da7c18f35cc5ae0912ace389cf7c7721d65514ad
Slade <wiede_dani@bs-an.de>
2020-12-22 18:49:11 +0100

    can't wait for this to go wrong (add view for pmd quiz)

---
+++ sladebot/view/pmd_personality_quiz.php
@@ -0,0 +1,19 @@
+<?php require_once(__DIR__ . "/../frontend/pmd_personality_quiz/pmd_personality_front.php");
+$viewQuestions = new pmdFront(10)?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PMD Personality Quiz</title>
+    <link href="/sladebot/frontend/pmd_personality_quiz/pmd_personality_front.php">
+</head>
+<body>
+    <form id="questions">
+        <?php foreach ($viewQuestions as $viewQuestion) {?>
+        <h2><?= $viewQuestion["question"]?></h2><br>
+        <?php foreach ($viewQuestion["answers"] as $answer) {?>
+        <input type ="radio" id="<?=$answer?>"><label for="<?=$answer?>"><?=$answer?></label><br>
+        <?php }
+        }
+        ?>
+    </form>
+</body>
