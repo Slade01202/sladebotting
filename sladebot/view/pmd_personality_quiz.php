7a350a7c8a8cb4bbc0a14dde61748a800daeeff0
Slade <wiede_dani@bs-an.de>
2020-12-22 20:26:10 +0100

    remove debug

--- sladebot/view/pmd_personality_quiz.php
+++ sladebot/view/pmd_personality_quiz.php
@@ -7,8 +7,6 @@ $viewQuestions = new pmdFront(1)?>
     </head>
     <body>
         <form id="questions">
-            <h1>is this thing on?</h1>
-            <h1><?php var_dump($viewQuestions);?></h1>
             <?php foreach ($viewQuestions->getQuestionArray() as $viewQuestion) {?>
             <h2><?= $viewQuestion["question"]?></h2><br>
             <?php foreach ($viewQuestion["answers"] as $answer) {?>
