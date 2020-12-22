947dc0d036d1f445da74e7c05dedb05bf99af875
Slade <wiede_dani@bs-an.de>
2020-12-22 19:14:09 +0100

    its always sunny in debugland

--- sladebot/view/pmd_personality_quiz.php
+++ sladebot/view/pmd_personality_quiz.php
@@ -9,13 +9,7 @@ $viewQuestions = new pmdFront(10)?>
     <form id="questions">
         <h1>is this thing on?</h1>
         <h1><?php var_dump($viewQuestions)?></h1>
-        <?php foreach ($viewQuestions as $viewQuestion) {?>
-        <h2><?= $viewQuestion["question"]?></h2><br>
-        <?php foreach ($viewQuestion["answers"] as $answer) {?>
-        <input type ="radio" id="<?=$answer?>"><label for="<?=$answer?>"><?=$answer?></label><br>
-        <?php }
-        }
-        ?>
+
     </form>
 </body>
 </html>
