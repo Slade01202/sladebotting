0b453489ae1c5d4f075026a596c1da8e3483110f
Slade <wiede_dani@bs-an.de>
2020-12-22 18:56:53 +0100

    time for lots of debugging

--- sladebot/view/pmd_personality_quiz.php
+++ sladebot/view/pmd_personality_quiz.php
@@ -8,7 +8,8 @@ $viewQuestions = new pmdFront(10)?>
 </head>
 <body>
     <form id="questions">
-        <h1><?php var_dump($viewQuestions);?></h1>
+        <h1>is this thing on?</h1>
+        <h1><?php var_dump($viewQuestions)?></h1>
         <?php foreach ($viewQuestions as $viewQuestion) {?>
         <h2><?= $viewQuestion["question"]?></h2><br>
         <?php foreach ($viewQuestion["answers"] as $answer) {?>
