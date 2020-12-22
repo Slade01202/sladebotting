9718c33564142cd1869e9303d6cbed9e0392aed2
Slade <wiede_dani@bs-an.de>
2020-12-23 00:20:31 +0100

    being a pepega

--- sladebot/frontend/pmd_personality_quiz/pmd_personality_front.php
+++ sladebot/frontend/pmd_personality_quiz/pmd_personality_front.php
@@ -18,9 +18,6 @@ class pmdFront
     private function setQuestionArray($questionCount) {
         for ($i = 0; $i < $questionCount; $i++) {
             $this->questionStorage[$i] = $this->questions->getSingleQuestion();
-            while ($this->questionStorage[$i] === false) {
-                $this->questionStorage[$i] = $this->questions->getSingleQuestion();
-            }
         }
     }
 
