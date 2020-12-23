c0e5765d6f2246a90beedc4975f787ddd3be4a93
Nussr2 <topsecretorginization@gmail.com>
2020-12-23 02:02:24 -0500

    Dictionary initialization complete

--- sladebot/backend/Cipher/Cipher.py
+++ sladebot/backend/Cipher/Cipher.py
@@ -1,25 +1,33 @@
 """
 Desc: Encode and decode via keypad cipher (asymmetrical)
 
-input:
+imports:
 
 processing:
 
 output:
 """
+from pathlib import Path
 
 
+# takes dictionary file and returns it in array form
 def initialize_dictionary():
     dictionary = []
-    with open("sladebot/backend/Cipher/words.txt", "r") as filein:
+    with open("{}\\words.txt".format(Path(__file__).parent.absolute()), "r") as filein:
         for line in filein:
-            dictionary.append(line)
+            dictionary.append(line.strip())
     return dictionary
+
+
+def decode():
+    dic = initialize_dictionary()
+    print("decode")
+
+
+def encode():
+    dic = initialize_dictionary()
+    print("encode")
 
 
 def main():
     print("Cry")
-
-
-dic = initialize_dictionary()
-print(dic)
