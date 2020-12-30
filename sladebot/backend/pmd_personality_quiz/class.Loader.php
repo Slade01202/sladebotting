a659c8ee528ae2b16555b0f3f62e012990c94bfb
Slade <wiede_dani@bs-an.de>
2020-12-30 01:03:55 +0100

    add comments

--- sladebot/backend/pmd_personality_quiz/class.Loader.php
+++ sladebot/backend/pmd_personality_quiz/class.Loader.php
@@ -1,32 +1,65 @@
 <?php
 
+//Load YAML-Loader class
 require_once(__DIR__ . "/../../includes/Spyc.php");
+
+/**
+ * Class Loader
+ *
+ * class for loading questions in the backend, and returning singleton questions
+ */
 class Loader
 {
+    /**
+     * @var string path to the file containing all the questions + answers, usually filled by frontend
+     */
     private $questionPath;
 
+    /**
+     * @var array containing sub arrays with all question + answer text in them
+     */
     private $questions;
 
+    /**
+     * @var array containing the banned during question generation
+     */
     private $bannedIndex;
 
+    /**
+     * @param $questionFile string loads up all questions into an array
+     */
     private function setQuestions($questionFile) {
-        $this->questions= Spyc::YAMLLoad($questionFile);
+        $this->questions = Spyc::YAMLLoad($questionFile);
     }
 
+    /**
+     * @return int the number of questions in the $questions array
+     */
     private function questionCount() {
         return count($this->questions["questions"]);
     }
 
+    /**
+     * @param $questionPath string path to the file containing all questions
+     */
     private function setQuestionPath($questionPath) {
         $this->questionPath = $questionPath;
     }
 
+    /**
+     * Loader constructor.
+     * @param $questionPath string path to the file containing all questions
+     */
     public function __construct($questionPath) {
         $this->setQuestionPath($questionPath);
         $this->setQuestions($this->questionPath);
         $this->bannedIndex = [];
     }
 
+    /**
+     * @return bool|array shouldn't return false, currently does, returns the subarray taken from $questions with the given index
+     * TODO: make this actually work :V
+     */
     public function getSingleQuestion() {
         $randIndex = rand(0, ($this->questionCount() - 2));
         if (!isset($randIndex, $bannedIndex)) {
--- sladebot/frontend/pmd_personality_quiz/pmd_personality_front.php
+++ sladebot/frontend/pmd_personality_quiz/pmd_personality_front.php
@@ -2,23 +2,44 @@
 
 require_once (__DIR__ . "/../../backend/pmd_personality_quiz/class.Loader.php");
 
+/**
+ * Class pmdFront
+ * Contains methods for handling questions received by the backend Loader class, preparing them for end user view
+ */
 class pmdFront {
 
+    /**
+     * @var Loader contains a backend Loader object
+     */
     private $questions;
 
+    /**
+     * @var array contains the questions ready to be sent to the view
+     */
     private $questionStorage = [];
 
+    /**
+     * pmdFront constructor.
+     * @param $questionCount int amount of questions to be pulled from the backend
+     * TODO: add check to validate count before executing
+     */
     public function __construct($questionCount) {
         $this->questions = new Loader(__DIR__ . "/../../backend/pmd_personality_quiz/questions.yaml");
         $this->setQuestionArray($questionCount);
     }
 
+    /**
+     * @param $questionCount int amount of questions to be pulled from the backend
+     */
     private function setQuestionArray($questionCount) {
         for ($i = 0; $i < $questionCount; $i++) {
             $this->questionStorage[$i] = $this->questions->getSingleQuestion();
         }
     }
 
+    /**
+     * @return array the array containing the questions from the frontend, ready to be displayed in view
+     */
     public function getQuestionArray() {
         return $this->questionStorage;
     }
