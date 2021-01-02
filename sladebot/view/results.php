771646734f55448cce1abe5b2adbed182e196a0c
Slade <wiede_dani@bs-an.de>
2021-01-03 00:43:05 +0100

    debug, testing

--- sladebot/view/results.php
+++ sladebot/view/results.php
@@ -1,3 +1,8 @@
+<?php
+require_once(__DIR__ . "/../frontend/pmd_personality_quiz/pmd_result_front.php");
+$fuck = new resultFront();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +12,8 @@
 <h1> The results are in!</h1>
 <div>
     <p>Looks like you are a ... or ...!</p>
+
+    <p><?php $fuck->debug();?></p>
 </div>
 </body>
 </html>
